a8581dbdab453dfa9b9ca9d4ce478f77e914bfd3
cole <bernankez@qq.com>
2022-06-16 01:19:22 +0800

    add Vue.use

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import "./style.css";
 import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js";
 import Vuex, { store } from "./Vuex";
-import { a, isLoop } from "./interviews/is-loop";
+import { initVueUse } from "./interviews/vue-use";
 
+initVueUse(Vue);
 Vue.use(Vuex);
 
 // const a = Vue.observable({
@@ -14,4 +15,4 @@ const vm = new Vue({
   render: h => h("div", store.getters.c),
 }).$mount("#app");
 
-console.log(isLoop(a));
+console.log(vm.$store)
