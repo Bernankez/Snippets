320d4a12f1e4ca6f9ff25db48d0bee06053695f1
shengbc <shengbeichen@inspur.com>
2022-06-17 17:25:40 +0800

    add nextTick scheduler, add reactive.spec

--- src/interviews/vue-nextTick.ts
+++ src/interviews/vue-nextTick.ts
@@ -1,8 +1,8 @@
 const p = Promise.resolve();
 
 export function initVueNextTick(Vue) {
-  Vue.nextTick = function (cb?: Function) {
-    return cb ? p.then(() => cb()) : p;
+  Vue.nextTick = function (cb?: () => void) {
+    return cb ? p.then(cb) : p;
   };
 
   Vue.prototype.$nextTick = Vue.nextTick;
