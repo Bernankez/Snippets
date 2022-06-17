fb99a7722612bedc8d8d2678ecb050a4ef3fcef4
cole <bernankez@qq.com>
2022-06-17 10:58:52 +0800

    add vitest

--- src/interviews/is-loop.ts
+++ src/interviews/is-loop.ts
@@ -1,4 +1,4 @@
-function ListNode(this: LIST_NODE, val: number) {
+export function ListNode(this: LIST_NODE, val: number) {
   this.val = val;
   this.next = null;
 }
@@ -7,12 +7,6 @@ type LIST_NODE = {
   val: any;
   next: LIST_NODE | null;
 };
-
-export let a = new ListNode(4);
-let b = (a.next = new ListNode(1));
-let c = (b.next = new ListNode(8));
-let d = (c.next = new ListNode(4));
-let e = (d.next = b);
 
 export function isLoop(node: LIST_NODE) {
   const nodeSet = new Set();
