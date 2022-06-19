d4312e70efc6f0ca0435246734275b6e01bb4eef
cole <bernankez@qq.com>
2022-06-19 18:46:40 +0800

    add new

---
+++ src/interviews/new-operator.spec.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import { People, New } from "./new-operator";
+
+const p = New(People, "some");
+const p1 = new People("some");
+
+describe("simulate new", () => {
+  it("equal", () => {
+    expect(p).toStrictEqual(p1);
+  });
+
+  it("instanceof", () => {
+    expect(p instanceof People).toBe(true);
+  });
+
+  it("prototype", () => {
+    expect(p1.__proto__).toBe(People.prototype);
+    expect(p.__proto__).toBe(People.prototype);
+  });
+});
---
+++ src/interviews/new-operator.ts
@@ -0,0 +1,15 @@
+export function New(fn: any, ...args) {
+  // 创建空对象
+  const newObj = {};
+  // 空对象原型指向构造函数原型
+  newObj.__proto__ = fn.prototype;
+  // 指定构造函数this
+  const rtn = fn.apply(newObj, args);
+  // 返回结果或新对象
+  return rtn || newObj;
+}
+
+export function People(this: { name: string; sex: string }, name: string) {
+  this.name = name;
+  this.sex = "male";
+}
