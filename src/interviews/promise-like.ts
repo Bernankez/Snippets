d1d2b81e062bbfb98ccc3c3d0e0fc18e61f4ff38
cole <bernankez@qq.com>
2022-06-21 00:34:32 +0800

    fix

--- src/interviews/promise-like.ts
+++ src/interviews/promise-like.ts
@@ -54,27 +54,69 @@ export class PromiseLike<V, R> {
     const onRejected = typeof onrejected === "function" ? onrejected : undefined;
 
     // 对于每个then 返回一个Promise
-    return new PromiseLike((resolve, reject) => {
+    const rtn = new PromiseLike((resolve, reject) => {
       let result;
       const callback = () => {
-        // 当then执行时promise未被决议
-        // TODO 当then执行时promise已被决议
-        if (this._state === PromiseState.FULFILLED && onFulfilled) {
-          result = onFulfilled.call(null, this._result);
-        } else if (this._state === PromiseState.REJECTED && onRejected) {
-          result = onRejected.call(null, this._result);
+        try {
+          // 当then执行时promise未被决议
+          if (this._state === PromiseState.FULFILLED) {
+            if (onFulfilled) {
+              result = onFulfilled.call(undefined, this._result);
+            } else {
+              resolve(this._result);
+            }
+          } else if (this._state === PromiseState.REJECTED) {
+            if (onRejected) {
+              result = onRejected.call(undefined, this._result);
+            } else {
+              reject(this._result);
+            }
+          }
+        } catch (e) {
+          reject(e);
         }
-        if (result && typeof result.then === "function") {
+        if ((result && "then" in result) || typeof result === "function") {
           // 执行结果如果仍是thenable的，则向下传递resolve,reject，新一轮微任务中执行
-          result.then(resolve, reject);
+          if (result === rtn) {
+            reject(new TypeError("PromiseLike: the value or reason cannot be promise it self."));
+          } else if ("then" in result) {
+            const _resolve = value => {
+              try {
+                if (typeof value === "object" && value && "then" in value) {
+                  value.then(_resolve, _reject);
+                } else {
+                  resolve(value);
+                }
+              } catch (e) {
+                reject(e);
+              }
+            };
+            const _reject = reason => {
+              reject(reason);
+            };
+            try {
+              result.then(_resolve, _reject);
+            } catch (e) {
+              reject(e);
+            }
+          } else {
+            result.call(result, this._result, resolve, reject);
+          }
         } else {
           // 返回fulfilled的result
           resolve(result);
         }
       };
-
-      this._callback.push(callback);
+      if (this._state !== PromiseState.PENDING) {
+        // 当promise已经被决议时，进入微任务列表执行
+        queueMicrotask(() => {
+          callback();
+        });
+      } else {
+        this._callback.push(callback);
+      }
     });
+    return rtn;
   }
 }
 
