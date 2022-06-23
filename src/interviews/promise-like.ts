140ecca0910d301bb5e1fabdbf69e19974d4c5d6
cole <bernankez@qq.com>
2022-06-24 01:17:53 +0800

    ?

--- src/interviews/promise-like.ts
+++ src/interviews/promise-like.ts
@@ -142,4 +142,4 @@ export const adapter = {
     };
   },
 };
-module.exports = adapter;
+// module.exports = adapter;
--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,87 @@
 import "./style.css";
 // @ts-ignore
 // import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js";
-import { PromiseLike, adapter } from "@/interviews/promise-like";
+import { PromiseLike } from "@/interviews/promise-like";
 
-function test(value) {
-  debugger
-  return new PromiseLike((resolve, reject) => resolve(value));
-}
+const app = document.querySelector("#app");
+console.log(app);
+const fragment = document.createDocumentFragment();
+const button1 = document.createElement("button");
+const button2 = document.createElement("button");
+fragment.appendChild(button1);
+fragment.appendChild(button2);
+app?.appendChild(fragment);
+button1.innerText = "button1";
+button1.addEventListener("click", promiselike);
+button2.innerText = "button2";
+button2.addEventListener("click", promise);
 
-function testInner(value) {
-  return {
-    then: function (onFulfilled) {
-      debugger;
-      setTimeout(function () {
-        onFulfilled(value);
-      }, 0);
-    },
-  };
-}
-
-function xFactory() {
-  return {
-    then: function (resolvePromise) {
-      resolvePromise(yFactory());
-    },
-  };
-}
-
-function yFactory() {
-  debugger;
-  return test(testInner({ a: 1 }));
-}
-
-const { promise, resolve, reject } = adapter.deferred();
-resolve();
-const step = promise.then(() => {
-  return xFactory();
-});
-debugger;
-console.log(
-  step.then(res => {
-    console.log(res);
+function promiselike() {
+  console.clear();
+  new PromiseLike((resolve, reject) => {
+    resolve();
   })
-);
-// console.log(
-//   new PromiseLike((resolve, reject) => {
-//     resolve({ a: 1 });
-//   })
-// );
+    .then(() => {
+      console.log(0);
+      return new PromiseLike((resolve, reject) => {
+        resolve(4);
+      });
+    })
+    .then(res => {
+      console.log(res);
+    });
+
+  new PromiseLike((resolve, reject) => {
+    resolve();
+  })
+    .then(() => {
+      console.log(1);
+    })
+    .then(() => {
+      console.log(2);
+    })
+    .then(() => {
+      console.log(3);
+    })
+    .then(() => {
+      console.log(5);
+    })
+    .then(() => {
+      console.log(6);
+    });
+}
+
+function promise() {
+  console.clear();
+  new Promise<void>((resolve, reject) => {
+    resolve();
+  })
+    .then(() => {
+      console.log(0);
+      return new Promise((resolve, reject) => {
+        resolve(4);
+      });
+    })
+    .then(res => {
+      console.log(res);
+    });
+
+  new Promise<void>((resolve, reject) => {
+    resolve();
+  })
+    .then(() => {
+      console.log(1);
+    })
+    .then(() => {
+      console.log(2);
+    })
+    .then(() => {
+      console.log(3);
+    })
+    .then(() => {
+      console.log(5);
+    })
+    .then(() => {
+      console.log(6);
+    });
+}
