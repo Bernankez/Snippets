b74153c121bf0896a15ce9aee4616dd04eb949f7
cole <bernankez@qq.com>
2022-08-14 20:58:02 +0800

    refactor: vite3

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+/// <reference types="vitest" />
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": resolve(__dirname, "src")
     },
   },
 });
